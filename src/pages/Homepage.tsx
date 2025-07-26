59157df8dd1a082c1466827eae93911909d725a2
AlysonRTY <variationsforpiano@gmail.com>
2025-07-26 22:40:28 +0200

    shadcn setup

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function Homepage() {
   return (
     <>
       <h1>Homepage Hi guys i like react</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }

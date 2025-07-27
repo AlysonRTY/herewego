a5d3131fbd8b1bca7f4d2950e8e1bbe0b0aad9e8
AlysonRTY <variationsforpiano@gmail.com>
2025-07-27 23:24:36 +0200

    first pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Homepage from "./pages/Homepage";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Homepage />} />
+          <Route path="aboutMe" element={<AboutMe />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,8 @@
+function AboutMe() {
+  return (
+    <>
+      <h1>zzzz</h1>
+    </>
+  );
+}
+export default AboutMe;

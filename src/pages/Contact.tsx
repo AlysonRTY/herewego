79af74ae3f23b93b21553e1c926694a8e603df04
AlysonRTY <variationsforpiano@gmail.com>
2025-09-07 18:43:35 +0100

    Enhance Contact page with layout

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,12 @@
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Contact</h1>
+      <p>
+        I'm always looking for new opportunities and collaborations. Feel free
+        to reach out to me via email or through the contact form below.
+      </p>
+      <form></form>
+    </div>
+  );
 }
